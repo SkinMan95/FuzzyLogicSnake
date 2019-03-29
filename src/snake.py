5956c029652660d84d4c731d3329d80d277aec1e
Alejandro Anzola Avila <anzola95avila@gmail.com>
2019-03-29 13:07:50 -0500

    cambios varios

--- src/snake.py
+++ src/snake.py
@@ -2,8 +2,20 @@ import logging
 import random
 import copy
 import time
-import threading
+import numpy as np
+
 grid = None
+
+
+def sum_pos(a, b):
+	assert len(a) == len(b)
+	r = [0] * len(a)
+	for i in range(len(a)):
+		r[i] = a[i] + b[i]
+
+	print("{} + {} = {}".format(a, b, r))
+	return r
+
 
 class Element(object):
 	def __init__(self, init_pos=None):
@@ -18,24 +30,22 @@ class Element(object):
 	
 
 class MoveElement(Element):
-	directions = [(1,0),(0,-1),(-1,0),(0,1)]
+	directions = [(1, 0), (0, -1), (-1, 0), (0, 1)]
 
-	def __init__(self, init_pos,direction):
+	def __init__(self, init_pos, direction=None):
 		super(MoveElement, self).__init__(init_pos)
-		self.direction = random.choice(MoveElement.directions)
-		self.lastPosition  = None
+		self.direction = random.choice(MoveElement.directions) if direction is None else direction
+		self.lastPosition = None
 		
 	def move(self, dir):
-		lastPosition = copy.deepcopy(self.pos)
-		npos = self.pos
-		for i in range(len(dir)):
-			npos[i] += dir[i]
-		self.pos = npos  
+		self.lastPosition = copy.deepcopy(self.pos)
+		self.set_pos(sum_pos(self.pos, dir))
 	
 
 class Target(Element):
 	def __repr__(self):
 		return "O"
+
 
 class Obstacle(Element):
 	def __repr__(self):
@@ -44,19 +54,20 @@ class Obstacle(Element):
 
 class Snake(MoveElement):
 	global grid
+
 	def __init__(self, initpos):
-		# super(Snake, self).__init__(*args)) # stemen que inventa :v
 		super(Snake, self).__init__(initpos, random.choice(MoveElement.directions))
 		self.tail = None
 
 	def grow_tail(self):
-		self.tail = TailElement(initPos=self.pos) if self.tail is None else TailElement(initPos=self.pos, tail=self.tail)
+		self.tail = TailElement(init_pos=self.pos) if self.tail is None else TailElement(init_pos=self.pos, tail=self.tail)
 		return self.tail
 
-	def move(self):  	
-		# super(Snake, self).move(dir)
-		if self.tail != None:
-			self.tail.set
+	def move(self, d):
+		lpos = self.get_pos()
+		super(Snake, self).move(d)
+		if self.tail is not None:
+			self.tail.move(lpos)
 
 	def get_tail(self):
 		return self.tail
@@ -66,28 +77,33 @@ class Snake(MoveElement):
 
 	def __repr__(self):
 		return "*"
-				
+
+
 class TailElement(MoveElement):
-	def __init__(self, initPos=None, tail=None):
-		super(TailElement, self).__init__(initPos)
-		self.tail=None
+	def __init__(self, init_pos=None, tail=None):
+		super(TailElement, self).__init__(init_pos)
+		self.tail = None
 
 	def __repr__(self):
 		return "."
 
+	def get_next(self):
+		return self.tail
+
 	def move(self, pos):
-		lastMove = self.pos
+		last_move = self.pos
 		self.set_pos(pos)
-		if tail:
-			self.tail.move(lastMove)
+		if self.tail:
+			self.tail.move(last_move)
 				
 
 def gen_random_pos(lim):
 	return [random.randint(2, lim-2), random.randint(2, lim-2)]
 
+
 class Grid(object):
-  
-	def gen_random_grid(self, dimensions, snake, nobstacles, target=None):
+	@staticmethod
+	def gen_random_grid(dimensions, snake, nobstacles, target=None):
 		elements = [Obstacle() for i in range(nobstacles)]
 
 		if target is None:
@@ -99,13 +115,13 @@ class Grid(object):
 		grid = [[None for i in range(dimensions)] for j in range(dimensions-2)]
 
 		for i in grid:
-			i[0]=Obstacle()
-			i[dimensions-1]=Obstacle()
-		grid.insert(0,[Obstacle() for i in range(dimensions)])
+			i[0] = Obstacle()
+			i[-1] = Obstacle()
+		grid.insert(0, [Obstacle() for i in range(dimensions)])
 		grid.append([Obstacle() for i in range(dimensions)])
 
-		pos = snake.get_pos()
-		grid[pos[0]][pos[1]] = snake
+		# pos = snake.get_pos()
+		# grid[pos[0]][pos[1]] = snake
 
 		pos = target.get_pos()
 		grid[pos[0]][pos[1]] = target
@@ -121,7 +137,6 @@ class Grid(object):
 
 		return grid
 
-
 	def __init__(self, dimensions, initial_grid=None, nobstacles=None, snake=None):
 		assert isinstance(dimensions, int)
 		assert (nobstacles is None) or (nobstacles < dimensions**2)
@@ -130,33 +145,79 @@ class Grid(object):
 		self.nobstacles = random.randint(1, dimensions//2) if nobstacles is None else nobstacles
 		self.snake = snake if snake is not None else Snake(gen_random_pos(self.dimensions))
 
-		self.grid = self.gen_random_grid(self.dimensions, self.snake, self.nobstacles) if initial_grid is None else initial_grid
+		self.grid = Grid.gen_random_grid(dimensions=self.dimensions, snake=self.snake, nobstacles=self.nobstacles, target=None) if initial_grid is None \
+			else initial_grid
 
-	def nextMove(self):
-		self.snake.move()
-		# cords = self.snake.get_head_pos()
-		# print(cords)
-		pass
+	def can_move(self, pos, snake):
+		can = self.grid[pos[0]][pos[1]] is None
+
+		tail = snake.get_tail()
+		while can and tail is not None:
+			can = pos != tail.get_pos()
+			tail = tail.get_next()
+
+		return can
+
+	def takeStep(self) -> bool:
+		spos = self.snake.get_pos()
+		dirs = MoveElement.directions[:] # copy
+		random.shuffle(dirs)
+		moved = False
+		for d in dirs:
+			npos = sum_pos(spos, d)
+			if self.can_move(npos, self.snake):
+				self.snake.move(d)
+				moved = True
+
+		if not moved:
+			print("Can't step any further")
+
+		return moved
+
+	def next_move(self, iterations=None):
+		assert isinstance(iterations, int) or iterations is None
+		assert iterations is None or iterations > 0
+
+		iterate = True
+		while iterate and (iterations is None or iterations > 0):
+			print(self)
+			iterate = self.takeStep()
+
+			iterations = iterations-1 if iterations is not None else None
+
+		print(self)
 
 	def __str__(self):
 		r = ""
-		for i in self.grid:
-			for j in i:
-				r += str(j) if j != None else " "
+		# print("pos:", self.snake.get_pos())
+
+		grid = self.grid
+		for i in range(len(grid)):
+			for j in range(len(grid[i])):
+				# print((i, j))
+				if [i, j] == self.snake.get_pos():
+					r += "*"
+				else:
+					r += str(grid[i][j]) if grid[i][j] is not None else " "
 			r += "\n"
 		return r			
 
+
 def example(grid):
 	for i in range(10):
-		print(grid)
+		# print(grid)
 		print(i)
-	grid.nextMove()
-	time.sleep(1)
+		grid.next_move()
+		time.sleep(1)
+
+
 
 
 def main():
 	global grid
 	grid = Grid(10)
-	threading.Thread(target=example(grid)).start()
+	example(grid)
+
+
 if __name__ == "__main__":
 	main()
